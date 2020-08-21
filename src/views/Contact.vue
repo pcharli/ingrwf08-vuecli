4a573e549b96c89aabe789987e0c26da8b934e34
Pierre Charlier <lretlo@gmail.com>
2020-08-21 14:00:46 +0200

    initi

---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+    <p>Texte</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+    h1{
+        color: $titre;
+    }
+</style>
